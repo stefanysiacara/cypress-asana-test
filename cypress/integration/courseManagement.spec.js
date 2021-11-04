ebe91d4227e64f9f9d7598f376e309550c85729c
Stefany.Siacara <stefany.siacara@jalasoft.com>
2021-11-04 17:54:25 -0400

    Adding and remove course

---
+++ cypress/integration/courseManagement.spec.js
@@ -0,0 +1,32 @@
+import { login } from "../support/login.js";
+import { coursesList } from "../support/coursesList.js";
+
+const COURSE_LIST = '[data-test-id="modal-backdrop"]';
+const COURSE_DASHBOARD = 'div[data-test-id="classes-dashboard"]';
+
+describe('Course Management', () => {
+  it('Verify we can add a new course to our course list', () => {
+    const course = "Arithmetic";
+    
+    cy.visit('/');
+    login.userLogin();
+    coursesList.editCoursesButton();
+    coursesList.addCourse(course);
+    coursesList.continueButton();
+    cy.get(COURSE_LIST,{timeout:10000}).should('not.be.visible');
+    cy.get(COURSE_DASHBOARD).contains('h3',course);
+  });
+  it('Verify we can remove a course to our course list', () => {
+    const course = "Arithmetic";
+    
+    cy.visit('/');
+    login.userLogin();
+    coursesList.editCoursesButton();
+    coursesList.removeCourse(course);
+    coursesList.continueButton();
+    cy.get(COURSE_LIST,{timeout:10000}).should('not.be.visible');
+    cy.get(COURSE_DASHBOARD).contains('h3',course).should('not.contain');
+  });
+
+
+});
---
+++ cypress/support/coursesList.js
@@ -0,0 +1,26 @@
+const EDIT_COURSES_BUTTON = 'button[class=_12iepmsr]';
+const CONTINUE_BUTTON = 'button[data-test-id="class-picker-modal-continue"]';
+const PANEL_COURSE_LIST = 'div[class="_n4d7gpg"]';
+
+
+export const coursesList = {
+  editCoursesButton()
+  {
+    //cy.wait(15000);
+    cy.get(EDIT_COURSES_BUTTON,{timeout:15000}).should("be.visible")
+    cy.get(EDIT_COURSES_BUTTON).click();
+  },
+  addCourse(checkname){
+    cy.get(PANEL_COURSE_LIST,{timeout:10000}).should('be.visible');
+    cy.contains('label', checkname).children('div').children('input').check();
+  },
+  removeCourse(checkname){
+    cy.get(PANEL_COURSE_LIST,{timeout:10000}).should('be.visible');
+    cy.contains('label', checkname).children('div').children('input').uncheck();
+  },
+  continueButton()
+  {
+    cy.wait(4000);
+    cy.contains(CONTINUE_BUTTON,'Continue').click();
+  }
+}
